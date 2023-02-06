5b7c510fd3d721a7726fb1e78016931671cdff9d
sfolador <sfolador@gmail.com>
2023-02-06 18:31:15 +0100

    ran pint

--- config/ai-email-suggest.php
+++ config/ai-email-suggest.php
@@ -5,5 +5,5 @@ return [
     'model' => 'text-davinci-003',
     'openai_key' => env('OPENAI_KEY'),
     'default_response' => 'Maybe you meant %suggestion%?',
-    'default_route' => 'ai-email-suggest'
+    'default_route' => 'ai-email-suggest',
 ];
