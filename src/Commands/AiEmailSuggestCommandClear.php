4156d51c6f84be0a161219ad0f190c47989fbb56
sfolador <sfolador@gmail.com>
2023-02-09 18:57:33 +0100

    added command for cache clear

---
+++ src/Commands/AiEmailSuggestCommandClear.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Sfolador\AiEmailSuggest\Commands;
+
+use Illuminate\Console\Command;
+
+class AiEmailSuggestCommandClear extends Command
+{
+    protected $signature = 'email-suggest:clear';
+
+    protected $description = 'Clears the cache of the email suggestions';
+
+    public function handle(): void
+    {
+        $this->info("WIP");
+    }
+}
